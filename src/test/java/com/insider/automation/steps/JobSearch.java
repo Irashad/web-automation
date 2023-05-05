8be23ac675c0af6ce07ab3a3c8f1037b3c17953e
Irashad <30791173+Irashad@users.noreply.github.com>
2023-05-06 02:29:18 +0400

    Feature/improvements (#3)
    * improvement on reporting
    
    * minor improvements on reporting

--- src/test/java/com/insider/automation/steps/JobSearch.java
+++ src/test/java/com/insider/automation/steps/JobSearch.java
@@ -1,5 +1,6 @@
 package com.insider.automation.steps;
 
+import com.aventstack.extentreports.Status;
 import com.insider.automation.pageobjects.web.CareersPage;
 import com.insider.automation.pageobjects.web.HomePage;
 import com.insider.automation.pageobjects.web.JobsPage;
@@ -17,11 +18,11 @@ public class JobSearch extends BaseSteps {
         helper.getAcceptAllCookies().click();
         homePage.getMoreMenu().click();
         homePage.goToCareersPage();
-
     }
 
     @Test(priority = 2,dependsOnMethods = "homePageTest")
     public void careersPageTest() {
+
         CareersPage careersPage = getPages().getCareersPage();
         Assert.assertTrue(careersPage.isLoaded());
 
@@ -42,7 +43,10 @@ public class JobSearch extends BaseSteps {
     public void jobsPageTest() {
         JobsPage jobsPage = getPages().getCareersPage().goToQaJobTitle();
         jobsPage.getSeeAllQaJob().click();
+
         Assert.assertTrue(jobsPage.isFilterByLocationOptionsLoaded(), "options for location filter didn't loaded with in 20 seconds");
+
+        getReportLogger().log(Status.INFO,"Selecting  by Select class doesn't working on this element. Using different method");
         jobsPage.getFilterByLocationContainer().click();
         jobsPage.chooseLocation(Location.TR);
 
