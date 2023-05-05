9ff8741b98d26767134a6815e4f5fdf0b4e2be1e
Irashad <30791173+Irashad@users.noreply.github.com>
2023-05-05 06:19:47 +0400

    Feature/report (#2)
    * adding extent report to gradle
    implementing extent report
    implementing screenshot capture method
    adding listener to testng xml file
    
    * update readme.md file
    TestListener.java deleted
    
    * updated ExtentReportListener
    
    * updates on JobSearch , divided tests
    
    * updates on JobSearch , divided tests

---
+++ src/test/java/com/insider/automation/utils/Screenshot.java
@@ -0,0 +1,26 @@
+package com.insider.automation.utils;
+
+import com.insider.automation.session.DriverHolder;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Screenshot {
+    public static String capture(String testName) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+        Path screenshotPath = Paths.get("screenshots", testName + "_" + timeStamp + ".png");
+        Files.createDirectories(screenshotPath.getParent());
+
+        File screenshotFile = ((TakesScreenshot) DriverHolder.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
+        Files.copy(screenshotFile.toPath(), screenshotPath);
+
+        return screenshotPath.toString();
+    }
+}
