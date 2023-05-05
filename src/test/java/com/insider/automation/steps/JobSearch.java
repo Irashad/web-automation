7a06a1f677e7a96109f6f797c4d6ba992b90b7a6
Irashad <reshad.nesirov5@gmail.com>
2023-05-05 06:14:05 +0400

    updates on JobSearch , divided tests

--- src/test/java/com/insider/automation/steps/JobSearch.java
+++ src/test/java/com/insider/automation/steps/JobSearch.java
@@ -11,50 +11,48 @@ import org.testng.annotations.Test;
 
 public class JobSearch extends BaseSteps {
     @Test(priority = 1)
-    public void endToEndTestsForJobSearch() {
-        // test 1 validate home page loaded
+    public void homePageTest() {
         HomePage homePage = getPages().getHomePage().openLandingPage();
         Assert.assertTrue(homePage.isLoaded());
         helper.getAcceptAllCookies().click();
-
-        // test 2.2 validate careers page opening when clicking
         homePage.getMoreMenu().click();
-        CareersPage careersPage = getPages().getHomePage().goToCareersPage();
+        homePage.goToCareersPage();
+
+    }
+
+    @Test(priority = 2)
+    public void careersPageTest() {
+        CareersPage careersPage = getPages().getCareersPage();
         Assert.assertTrue(careersPage.isLoaded());
 
-        // test 2.3 validate life insider block is opened
-        int lifeAtInsiderElementListSize = careersPage.getSizeOfLifeAtInsiderBlock();
-        Assert.assertTrue(helper.sizeIsBiggerThanZero(lifeAtInsiderElementListSize));
+        int lifeAtInsiderBlockElementSize = careersPage.getSizeOfLifeAtInsiderBlock();
+        Assert.assertTrue(helper.sizeIsBiggerThanZero(lifeAtInsiderBlockElementSize));
 
-        // test 2.4 validate locations block is opened
-        int locationsElementListSize = careersPage.getSizeOfLocationsBlock();
-        Assert.assertTrue(helper.sizeIsBiggerThanZero(locationsElementListSize));
+        int locationsBlockElementSize = careersPage.getSizeOfLocationsBlock();
+        Assert.assertTrue(helper.sizeIsBiggerThanZero(locationsBlockElementSize));
 
-        // test 2.5 validate teams block is opened
-        int teamsElementListSize = careersPage.getSizeOfTeamsBlock();
-        Assert.assertTrue(helper.sizeIsBiggerThanZero(teamsElementListSize));
+        int teamsBlockElementSize = careersPage.getSizeOfTeamsBlock();
+        Assert.assertTrue(helper.sizeIsBiggerThanZero(teamsBlockElementSize));
 
-        // test 3 click see all teams
         careersPage.seeAllTeams();
 
-        // test 4 click QA Assurance:
-        JobsPage jobsPage = careersPage.goToQaJobTitle();
+    }
 
-        // test 5 QA page. Click see all qa job
+    @Test(priority = 3)
+    public void jobsPageTest() {
+        JobsPage jobsPage = getPages().getCareersPage().goToQaJobTitle();
         jobsPage.getSeeAllQaJob().click();
-
-        //test 6 filter by location
         Assert.assertTrue(jobsPage.isFilterByLocationOptionsLoaded(), "options for location filter didn't loaded with in 20 seconds");
         jobsPage.getFilterByLocationContainer().click();
         jobsPage.chooseLocation(Location.TR);
 
-        //test 8 click apply now  and verify new tab opened with expected pre-url
+        // Click 'apply now'  and verify new tab opened with expected pre-url
         String mainWindowHandle = DriverHolder.getInstance().getDriver().getWindowHandle();
         jobsPage.clickApllyButtonWithJS();
         Assert.assertTrue(DriverHolder.getInstance().waitForNumberofWindowsToBe(2, 8));
-
         helper.switchWindowNextTab(mainWindowHandle);
         Assert.assertTrue(DriverHolder.getInstance().waitForUrlContains("jobs.lever.co/useinsider", 10));
+
     }
 
 }
